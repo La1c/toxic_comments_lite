e69cd401109cc4dbb27555f4c517c8f4022b4455
La1c <vladimir.ageev@me.com>
2019-09-30 17:55:00 +0300

    prepare stage changed

--- src/prepare.py
+++ src/prepare.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+from utils import try_mkdir
 
 def clean_df(df, column_name):
     """This function removes line breaks from the specified column
@@ -19,21 +20,19 @@ if __name__ == "__main__":
     import sys
     import os
 
-    input_df_path = sys.argv[1]
-    input_file_name, extention = input_df_path.split(os.path.sep)[-1].split('.')
-    output_file_name = '.'.join([input_file_name + '_prepared', extention])
-    output_path = os.path.join('data', 'prepared')
+    input_df_paths = sys.argv[1:]
+    for input_df_path in input_df_paths:
+        input_file_name, extention = input_df_path.split(os.path.sep)[-1].split('.')
+        output_file_name = '.'.join([input_file_name + '_prepared', extention])
+        output_path = os.path.join('data', 'prepared')
 
-    try:
-        os.makedirs(output_path)
-    except FileExistsError as e:
-        pass
+        try_mkdir(output_path)
 
-    print('processing {}'.format(input_df_path))
-    df = pd.read_csv(input_df_path)
-    df_clean = clean_df(df, 'comment_text')
-    print('writing {} to {}'.format(output_file_name, output_path))
-    df_clean.to_csv(os.path.join(output_path, output_file_name), index=False)
+        print('processing {}'.format(input_df_path))
+        df = pd.read_csv(input_df_path)
+        df_clean = clean_df(df, 'comment_text')
+        print('writing {} to {}'.format(output_file_name, output_path))
+        df_clean.to_csv(os.path.join(output_path, output_file_name), index=False)
 
 
     
