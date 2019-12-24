c4b81b6b9e43c93956c84ad70580b52f8ea0dfa9
La1c <vladimir.ageev@me.com>
2019-12-24 16:54:00 +0000

    aws creds; verbosity

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest
       
 RUN apt-get update \
-  && apt-get install -y python3-pip python3-dev \
+  && apt-get install -y python3-pip python3-dev git\
   && cd /usr/local/bin \
   && ln -s /usr/bin/python3 python \
   && pip3 install --upgrade pip
@@ -10,6 +10,8 @@ ADD . /toxic_comments
 WORKDIR /toxic_comments
 
 ENV MLFLOW_TRACKING_URI http://localhost:5000/
+ENV AWS_ACCESS_KEY_ID
+ENV AWS_SECRET_ACCESS_KEY
 
 RUN pip install -r requirements.txt
 
--- src/train.py
+++ src/train.py
@@ -32,21 +32,27 @@ if __name__ == "__main__":
                                      max_iter=1000,
                                      scoring=make_scorer(roc_auc_score))
         
+        print("Fitting lr for category {}".format(category))
         model.fit(features, data_df[category])
         
 
         with open(os.path.join(args.artifacts_path, '{}_lr.pkl'.format(category)), 'wb') as f:
+            print("Saving predictor locally for category {}".format(category))
             pickle.dump(model, f)
         
         try: 
             mlflow.set_experiment('/log_reg_cv_{}_{}'.format(category, datetime.today().strftime('%Y%m%d'))) 
             with mlflow.start_run():
+                print("Sending cv parameters and scores to ML Flow")
                 for i, c in enumerate(model.C_):
                     mlflow.log_param('C_{}'.format(i), c)
                     mlflow.log_metric("mean_roc_auc_C_{}".format(C_s[i]), np.mean(model.scores_[1][:, i]))  
-                    mlflow.log_artifact(os.path.join(args.artifacts_path, '{}_lr.pkl'.format(category)))     
-        except:
-            pass
+                
+                print("Sending model artifact to ML Flow")
+                mlflow.log_artifact(os.path.join(args.artifacts_path, '{}_lr.pkl'.format(category)))     
+        except Exception as e:
+            print(e)
+            
                 
 
             
