60cbc715555b9713de283bf4039db1238eb529c5
vladimirageev <vladimir_ageev@epam.com>
2020-01-23 17:52:09 +0300

    project restructure

--- run_container.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -i -p 8082:8082 -v /path/to/toxic_data:/toxic_comments/data -e AWS_ACCESS_KEY_ID=ABC -e AWS_SECRET_ACCESS_KEY=DEF --network=host toxic_classifier:latest train
